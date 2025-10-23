d5cf12d0fb229943221955fbaae554881316f023
Manuel Miranda <manuelin73cool@hotmail.com>
2025-10-22 23:17:20 -0500

    borre import innecesario

--- app/src/main/java/com/miranda/mitiendita360/ProductDetailActivity.kt
+++ app/src/main/java/com/miranda/mitiendita360/ProductDetailActivity.kt
@@ -35,13 +35,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.colorspace.WhitePoint
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.lifecycleScope
